0ecc1a52c8cf630eb678aea2abd48d656629cb4b
sukhman <coder4614@gmail.com>
2023-07-24 16:10:17 +0530

    email validation

--- script.js
+++ script.js
@@ -3,12 +3,20 @@ const submit=document.getElementById('submit')
 const names=document.getElementById('name')
 const email=document.getElementById('email')
 const userlist=document.getElementById('user-list')
+var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+
 submit.addEventListener('click',function(e){
     submit.style.opacity="0.7"
     e.preventDefault();
-    if(names.value==="" || email.value==""){
-        alert("fill your details")
-    }else{
+    if(names.value===""){
+        alert("name field cant be empty")
+    }
+    else if(!email.value.match(validRegex)){
+        alert("email is Invalid or empty")
+    }
+    
+    else{
         const li=document.createElement('li');
         li.appendChild(document.createTextNode(`NAME:${names.value} 
         EMAIL:${email.value}`))
